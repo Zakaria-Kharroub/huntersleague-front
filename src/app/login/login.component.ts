e6447baa2393406e8f5d80897235675ca39d7327
zakaria <kharroubzakaria32@gmail.com>
2024-12-31 15:23:22 +0100

    login done with guards

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {AuthService} from "../services/auth.service";
+import {TokenService} from "../services/token.service";
+import {Router} from "@angular/router";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage:string='';
 
+  constructor(
+    private fb:FormBuilder,
+    private authservice:AuthService,
+    private tokenService:TokenService,
+    private router:Router
+  ) {
+    this.loginForm=this.fb.group({
+      login:['',Validators.required],
+      password:['',Validators.required]
+    });
+  }
+
+  onSubmit():void{
+    if (this.loginForm.valid){
+      this.authservice.login(this.loginForm.value).subscribe({
+        next:(response)=>{
+          this.tokenService.setToken(response.token);
+          this.router.navigate(['/']);
+        },
+        error:(error)=>{
+          console.error('login failed: ',error);
+          this.errorMessage='invalid credanteil.try again';
+        }
+      });
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -16,15 +16,22 @@ export interface RegisterRequest {
   nationality: string;
 }
 
+export interface LoginRequest{
+  login:string;
+  password:string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly API_URL = 'http://localhost:8443/api/v1/auth';
-  // ^ Changed to v1 to match backend
   constructor(private http: HttpClient) { }
 
   register(registerRequest: RegisterRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/register`, registerRequest);
+  }
+  login(loginRequest:LoginRequest):Observable<AuthResponse>{
+    return this.http.post<AuthResponse>(`${this.API_URL}/login`,loginRequest);
   }
 }
