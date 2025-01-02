987061a35274d17cc60d39c8a4263c46e9aea2b8
zakaria <kharroubzakaria32@gmail.com>
2025-01-02 16:35:23 +0100

    archetect dashboard and getsion users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,13 @@ import {HomeComponent} from "./home/home.component";
 import {RegisterComponent} from "./register/register.component";
 import {LoginComponent} from "./login/login.component";
 import {authGuard} from "./guards/auth.guard";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {UsersComponent} from "./users/users.component";
 
 export const routes: Routes = [
   {path:'',component: HomeComponent  ,canActivate:[authGuard]}, // default route
   {path:'register', component: RegisterComponent},
   {path: 'login' ,component: LoginComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path:'users',component:UsersComponent}
 ];
